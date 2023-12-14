4519645d84e7a71426da249ecb73c9ea99021056
Rémi <neptunes92390@outlook.fr>
2023-12-14 16:46:10 +0100

    add function login and register user account JWT auth

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const server = express();
+const port = 3005;
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      title: "Secret Santa API",
+      description: "MDS API for Secret Santa",
+      contact: {
+        name: "Rémi",
+      },
+      servers: ["http://localhost:3005"],
+    },
+  },
+  apis: ["./routes/*.js"],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+server.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/secret-santa");
+
+server.use(express.urlencoded({extended:true}));
+server.use(express.json());
+
+
+const userRoute = require("./routes/userRoute");
+server.use("/", userRoute);
+
+
+
+server.listen(port, () => {
+  console.log(`app listening on port ${port}`);
+});
